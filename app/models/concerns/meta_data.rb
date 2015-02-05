6e034ce120950ac5196b6bb281fa802553c043b6
Matus Kmit <matus.kmit@zhdk.ch>
2015-02-05 16:26:32 +0100

    keywords in meta_data concern

--- app/models/concerns/meta_data.rb
+++ app/models/concerns/meta_data.rb
@@ -13,5 +13,13 @@ module Concerns
     def description
       meta_data.find_by(meta_key_id: 'description').try(:value)
     end
+
+    def keywords
+      Keyword
+        .joins(:meta_datum)
+        .where(meta_data: Hash[
+          :meta_key_id, 'madek:core:keywords',
+          "#{self.class.model_name.singular}_id".to_sym, id])
+    end
   end
 end
