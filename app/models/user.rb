ac3c46da418ca37953a0b84ed142bfe43522d1f4
Matus Kmit <matus.kmit@zhdk.ch>
2015-02-06 11:47:35 +0100

    user.shared_resource_with_users/groups?

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,4 +81,59 @@ class User < ActiveRecord::Base
     update!(usage_terms_accepted_at: nil)
   end
 
+  #############################################################
+
+  def shared_media_entry_with_groups?(media_entry)
+    media_entries
+      .joins(:group_permissions)
+      .where(media_entry_group_permissions:
+        { media_entry_id: media_entry.id,
+          get_metadata_and_previews: true })
+      .exists?
+  end
+
+  def shared_media_entry_with_users?(media_entry)
+    media_entries
+      .joins(:user_permissions)
+      .where(media_entry_user_permissions:
+        { media_entry_id: media_entry.id,
+          get_metadata_and_previews: true })
+      .exists?
+  end
+
+  def shared_collection_with_groups?(collection)
+    collections
+      .joins(:group_permissions)
+      .where(collection_group_permissions:
+        { collection_id: collection.id,
+          get_metadata_and_previews: true })
+      .exists?
+  end
+
+  def shared_collection_with_users?(collection)
+    collections
+      .joins(:user_permissions)
+      .where(collection_user_permissions:
+        { collection_id: collection.id,
+          get_metadata_and_previews: true })
+      .exists?
+  end
+
+  def shared_filter_set_with_groups?(filter_set)
+    filter_sets
+      .joins(:group_permissions)
+      .where(filter_set_group_permissions:
+        { filter_set_id: filter_set.id,
+          get_metadata_and_previews: true })
+      .exists?
+  end
+
+  def shared_filter_set_with_users?(filter_set)
+    filter_sets
+      .joins(:user_permissions)
+      .where(filter_set_user_permissions:
+        { filter_set_id: filter_set.id,
+          get_metadata_and_previews: true })
+      .exists?
+  end
 end
