448005d5c611994305096eaef9ef082e8599f26c
Matus Kmit <matus.kmit@zhdk.ch>
2015-02-09 14:41:10 +0100

    privacy status test for collection & filter_set

--- spec/presenters/filter_sets/filter_set_thumb_spec.rb
+++ spec/presenters/filter_sets/filter_set_thumb_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require Rails.root.join 'spec', 'presenters', 'shared', 'privacy_status_spec'
 
-describe Presenters::MediaEntries::MediaEntryThumb do
+describe Presenters::FilterSets::FilterSetThumb do
   it_responds_to 'privacy_status' do
-    let(:resource_type) { :media_entry }
+    let(:resource_type) { :filter_set }
   end
 end
