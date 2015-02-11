d2a2fb1609977add31f1a4bf894bb899c3063cab
Matus Kmit <matus.kmit@zhdk.ch>
2015-02-11 22:40:06 +0100

    dump tests for resources thumbs

--- spec/presenters/collections/collection_thumb_spec.rb
+++ spec/presenters/collections/collection_thumb_spec.rb
@@ -1,24 +1,14 @@
 require 'spec_helper'
 require Rails.root.join 'spec', 'presenters', 'shared', 'privacy_status_spec'
+require Rails.root.join 'spec', 'presenters', 'shared', 'dump_spec'
 
 describe Presenters::Collections::CollectionThumb do
-  def all_values(obj)
-    if obj.is_a?(Hash)
-      obj.values.map { |v| all_values(v) }.flatten
-    else
-      obj
-    end
+  it 'dummy' do
+    # for the rspec updator
   end
 
-  it 'dump' do
-    c = Collection.all.sample
-    p = described_class.new(c, c.responsible_user)
-    d = p.dump
-    expect(
-      all_values(d).all? do |v|
-        not v.nil? and not v.match(/ActiveRecord/)
-      end
-    ).to be true
+  it_can_be 'dumped' do
+    let(:object) { Collection.all.sample }
   end
 
   it_responds_to 'privacy_status' do
--- spec/presenters/media_entries/media_entry_thumb_spec.rb
+++ spec/presenters/media_entries/media_entry_thumb_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 require Rails.root.join 'spec', 'presenters', 'shared', 'privacy_status_spec'
+require Rails.root.join 'spec', 'presenters', 'shared', 'dump_spec'
 
 describe Presenters::MediaEntries::MediaEntryThumb do
   it 'dummy' do
     # for the rspec updator
+  end
+
+  it_can_be 'dumped' do
+    let(:object) { MediaEntry.all.sample }
   end
 
   it_responds_to 'privacy_status' do
