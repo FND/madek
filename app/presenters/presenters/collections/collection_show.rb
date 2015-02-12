6a426c626fa9f45741e472812f18dd0715a9cc05
Max F. Albrecht <1@178.is>
2015-02-12 16:43:28 +0100

    presenter/decorator: consistent poly_resources

--- app/presenters/presenters/collections/collection_show.rb
+++ app/presenters/presenters/collections/collection_show.rb
@@ -9,23 +9,25 @@ module Presenters
           .map { |me| Presenters::MediaEntries::MediaEntryThumb.new(me, @user) }
       end
 
-      def media_entries_thumbs
-        @resource \
-          .media_entries
-          .map { |me| Presenters::MediaEntries::MediaEntryThumb.new(me, @user) }
+      def poly_resources
+        {
+          media_entries:
+            @resource \
+              .media_entries
+              .map { |me| Presenters::MediaEntries::MediaEntryThumb.new(me, @user) },
+
+          collections:
+            @resource \
+              .collections
+              .map { |c| Presenters::Collections::CollectionThumb.new(c, @user) },
+
+          filter_sets:
+            @resource \
+              .filter_sets
+              .map { |fs| Presenters::FilterSets::FilterSetThumb.new(fs, @user) },
+        }
       end
 
-      def collections_thumbs
-        @resource \
-          .collections
-          .map { |c| Presenters::Collections::CollectionThumb.new(c, @user) }
-      end
-
-      def filter_sets_thumbs
-        @resource \
-          .filter_sets
-          .map { |fs| Presenters::FilterSets::FilterSetThumb.new(fs, @user) }
-      end
 
     end
   end
