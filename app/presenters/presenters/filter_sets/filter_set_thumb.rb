58956f64ab32d2d08f275471817b5981be3877f1
Max F. Albrecht <1@178.is>
2015-02-12 17:38:46 +0100

    wip

--- app/presenters/presenters/filter_sets/filter_set_thumb.rb
+++ app/presenters/presenters/filter_sets/filter_set_thumb.rb
@@ -1,6 +1,11 @@
 module Presenters
   module FilterSets
     class FilterSetThumb < Presenters::Shared::Resources::ResourcesThumb
+      def image_url(size = :small
+        binding.pry
+        
+        asset_path('dev_todo.png')
+      end
     end
   end
 end
